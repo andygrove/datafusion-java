c19a9471c005bce0b31470e8ad08f12277aa815c
Andy Grove <agrove@apache.org>
2026-05-10 09:25:12 -0600

    build: add Maven project skeleton with JUnit 5

---
+++ src/test/java/org/apache/datafusion/SanityTest.java
@@ -0,0 +1,11 @@
+package org.apache.datafusion;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SanityTest {
+    @Test
+    void mavenBuildWorks() {
+        assertEquals(2, 1 + 1);
+    }
+}
